a128f9e6940b48b05f3f7018740f5de8666af53d
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-12-23 02:08:11 +0530

    feat: Add logging for email connection command handler

--- slack/commands.js
+++ slack/commands.js
@@ -2,7 +2,10 @@ import { providerDetectModal } from "./views.js"
 
 export function registerCommands(slackApp) {
     slackApp.command("/connect-email", async ({ack,body,client}) => {
+        console.log("CONNECT EMAIL HANDLER HIT")
         await ack()
+        console.log("ACK SENT")
+        
         await client.views.open({
             trigger_id: body.trigger_id,
             view: providerDetectModal()
