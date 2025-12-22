5c268d441db1723974dc3fea2cccd9b9dde62224
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-12-23 02:44:23 +0530

    refactor: Standardize code formatting and improve readability in server.js

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
 import { googleOAuthRouter } from "./oauth/google.js";
 import { microsoftOAuthRouter } from "./oauth/microsoft.js";
-import { slackReceiver } from "./slack/app.js"
+import { slackReceiver } from "./slack/app.js";
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("INCOMING:", req.method, req.path)
-  next()
-})
+  console.log("INCOMING:", req.method, req.path);
+  next();
+});
 
-app.use(express.json());
+// Mount Slack receiver first so Bolt can verify signatures on the raw body
 app.use("/slack/events", slackReceiver.router);
-app.use("/oauth/google",googleOAuthRouter);
-app.use("/oauth/microsoft",microsoftOAuthRouter);
 
-app.listen(3000,() => {
-    console.log("Server is running on port 3000");
-})
+// Body parser for the rest of the app
+app.use(express.json());
+app.use("/oauth/google", googleOAuthRouter);
+app.use("/oauth/microsoft", microsoftOAuthRouter);
+
+app.use((err, req, res, next) => {
+  console.error("ERR:", err);
+  res.status(500).send("Server error");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
