a85ec8666116b2f01b7d12ffb14dbb501044c155
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-12-23 23:50:20 +0530

    fix: Correct return value for Gmail detection in providerDetector

--- utils/providerDetector.js
+++ utils/providerDetector.js
@@ -1,8 +1,8 @@
 export async function detectProvider(email) {
     const domain = email.split("@")[1]
     
-    if(domain.includes("gmail")|| domain.includes("google"))
-        return "google"
+    if(domain.includes("gmail") || domain.includes("google"))
+        return "gmail"
 
     if(domain.includes("outlook") || domain.includes("microsoft"))
         return "microsoft"
